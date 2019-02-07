9b754c6ba0901d37b0722a9cdf09a59d7dbb51cf
CJ Davis <cj.davis@cqlcorp.com>
2019-02-07 15:56:20 -0500

    Reduce app coupling

---
+++ src/apps/main.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const Main: React.SFC<{}> = props => <h1>Hello world!</h1>;
+
+export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { hot } from "react-hot-loader";
 
-import App from "./app";
+import Main from "./apps/main";
 
 // tslint:disable-next-line
 require("./styles/main.scss");
+
+const App = hot(module)(Main);
 
 ReactDOM.render(<App />, document.getElementById("app"));
